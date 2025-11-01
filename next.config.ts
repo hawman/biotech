427518d3f8fae6106f4f3788b40f1927e205b18c
hawman <44220972+hawman@users.noreply.github.com>
2025-11-02 05:22:08 +0800

    update nextconfig to debug

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,12 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: '/biotech',
+  assetPrefix: '/biotech',
   output: 'export',
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
