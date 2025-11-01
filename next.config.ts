62063fc75b10da6cdebb2d7918ff8952a34c46cf
hawman <44220972+hawman@users.noreply.github.com>
2025-11-02 04:45:50 +0800

    add config to publish to github pages

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '/biotech',
   output: 'export',
+  reactStrictMode: true,
 };
 
 export default nextConfig;
