4da9ed6d9ff51a05e7cd949b0c0fcabd9ede3301
hawman <44220972+hawman@users.noreply.github.com>
2025-11-03 21:41:03 +0800

    remove github pages related changes

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // basePath: '/biotech',
-  // assetPrefix: '/biotech',
-  output: 'export',
   reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
